ea0e6aa102938cc248023c87f233bcd6fefe2e59
Ryan Clark <ryan.clark.j@gmail.com>
2014-12-22 11:27:26 -0700

    add test for concurrency via pipes

--- test/index.test.js
+++ test/index.test.js
@@ -3,27 +3,48 @@ var monitor = require('./monitor');
 var Parallel = require('..');
 var util = require('util');
 var queue = require('queue-async');
+var stream = require('stream');
 
-test('reaches desired concurrency', function(t) {
-  function TestStream(concurrency, delay, options) {
-    this.monitor = monitor();
-    this.delay = Number(delay);
-    Parallel.call(this, concurrency, options);
+util.inherits(TestStream, Parallel);
+function TestStream(concurrency, delay, options) {
+  this.monitor = monitor();
+  this.delay = Number(delay);
+  Parallel.call(this, concurrency, options);
+}
+TestStream.prototype._process = function(chunk, enc, callback) {
+  var _this = this;
+  var finished = function(err, result) {
+    _this.push(result);
+    callback();
+  };
+  this.monitor.process(chunk, this.delay, finished);
+};
+
+util.inherits(Readable, stream.Readable);
+function Readable() {
+  this.chunk = 0;
+  stream.Readable.call(this);
+}
+Readable.prototype._read = function(size) {
+  var stream = this;
+
+  var chunk = this.chunk;
+  this.chunk = this.chunk + 1;
+
+  if (this.chunk === 51) {
+    stream.emit('finishWrites');
+    return this.push(null);
   }
 
-  util.inherits(TestStream, Parallel);
+  setTimeout(function() {
+    stream.push(chunk.toString());
+  }, 100);
+};
 
-  TestStream.prototype._process = function(chunk, enc, callback) {
-    var _this = this;
-    var finished = function(err, result) {
-      _this.push(result);
-      callback();
-    };
-    this.monitor.process(chunk, this.delay, finished);
-  };
-
+test('reaches desired concurrency w/writes', function(t) {
   var received = [];
   var testStream = new TestStream(10, 1000);
+
   testStream.on('data', function(data) {
     received.push(Number(data.toString()));
   });
@@ -38,9 +59,39 @@ test('reaches desired concurrency', function(t) {
   var q = queue(1);
   for (var i = 0; i < 50; i++) q.defer(write, i);
   q.await(function() {
-    testStream.end();
+    testStream.on('end', function() {
+      t.pass('fired end event');
+      t.equal(received.length, 50, 'received all chunks');
+
+      for (var j = 0; j < 50; j++) {
+        if (received.indexOf(j) === -1) t.fail('Did not receive chunk ' + j);
+      }
+
+      t.end();
+    });
+
     t.equal(testStream.monitor.concurrency(), 10, 'hit desired concurrency');
-    testStream.monitor.close();
+    testStream.end();
+  });
+});
+
+test('reaches desired concurrency w/pipes', function(t) {
+  var received = [];
+  var readable = new Readable();
+  var testStream = new TestStream(10, 1000);
+
+  readable.on('finishWrites', function() {
+    setImmediate(function() {
+      t.equal(testStream.monitor.concurrency(), 10, 'hit desired concurrency');
+    });
+  });
+
+  testStream.on('data', function(data) {
+    received.push(Number(data.toString()));
+  });
+
+  testStream.on('end', function() {
+    t.pass('fired end event');
     t.equal(received.length, 50, 'received all chunks');
 
     for (var j = 0; j < 50; j++) {
@@ -49,4 +100,6 @@ test('reaches desired concurrency', function(t) {
 
     t.end();
   });
+
+  readable.pipe(testStream);
 });
